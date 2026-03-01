eccbea76d68dffbf995eb31578540b389d2c3c35
Yoann Joseph <yoannjoseph16062000@gmail.com>
2026-03-01 10:27:20 +0100

    feat : add CommandPool and Commandbuffer

--- Engine3D/include/vulkan.h
+++ Engine3D/include/vulkan.h
@@ -245,6 +245,7 @@ private:
 
 private:
 	vk::raii::Queue queue = nullptr;
+	uint32_t queueIndex = ~0;
 
 private:
 	vk::raii::SurfaceKHR surface = nullptr;
@@ -269,6 +270,10 @@ private:
 private:
 	vk::raii::PipelineLayout pipelineLayout = nullptr;
 	vk::raii::Pipeline		 graphicsPipeline = nullptr;
+
+private:
+	vk::raii::CommandPool commandPool = nullptr;
+	vk::raii::CommandBuffer  commandBuffer = nullptr;
 
 private:
 
@@ -445,7 +450,6 @@ private:
 		std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
 
 		//get the first index into queueFamilyProperties which supports graphics
-		uint32_t queueIndex = ~0;
 		for (uint32_t qfpIndex = 0; qfpIndex < queueFamilyProperties.size(); qfpIndex++)
 		{
 			if ((queueFamilyProperties[qfpIndex].queueFlags & vk::QueueFlagBits::eGraphics) &&
@@ -719,6 +723,131 @@ private:
 
 		graphicsPipeline = vk::raii::Pipeline(device, nullptr, pipelineCreateInfoChain.get<vk::GraphicsPipelineCreateInfo>());
 	}
+	void transition_image_layout(
+		uint32_t imageIndex,
+		vk::ImageLayout oldLayout,
+		vk::ImageLayout newLayout,
+		vk::AccessFlags2 srcAccessMask,
+		vk::AccessFlags2 dstAccessMask,
+		vk::PipelineStageFlags2 srcStageMask,
+		vk::PipelineStageFlags2 dstStageMask
+	) 
+	{
+		vk::ImageMemoryBarrier2 barrier = 
+		{
+			.srcStageMask = srcStageMask,
+			.srcAccessMask = srcAccessMask,
+			.dstStageMask = dstStageMask,
+			.dstAccessMask = dstAccessMask,
+			.oldLayout = oldLayout,
+			.newLayout = newLayout,
+			.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.image = swapChainImages[imageIndex],
+			.subresourceRange = 
+			{
+				.aspectMask = vk::ImageAspectFlagBits::eColor,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1
+			}
+		};
+
+		vk::DependencyInfo dependencyInfo = 
+		{
+			.dependencyFlags = {},
+			.imageMemoryBarrierCount = 1,
+			.pImageMemoryBarriers = &barrier
+		};
+
+		commandBuffer.pipelineBarrier2(dependencyInfo);
+	}
+	void recordCommandBuffer(uint32_t imageIndex) 
+	{
+		commandBuffer.begin({});
+
+		// Transition the image layout for rendering
+		transition_image_layout(
+			imageIndex,
+			vk::ImageLayout::eUndefined,
+			vk::ImageLayout::eColorAttachmentOptimal,
+			{},
+			vk::AccessFlagBits2::eColorAttachmentWrite,
+			vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+			vk::PipelineStageFlagBits2::eColorAttachmentOutput
+		);
+
+		// Set up the color attachment
+		vk::ClearValue clearColor = vk::ClearColorValue(0.0f, 0.0f, 0.0f, 1.0f);
+		vk::RenderingAttachmentInfo attachmentInfo = 
+		{
+			.imageView = swapChainImageViews[imageIndex],
+			.imageLayout = vk::ImageLayout::eColorAttachmentOptimal,
+			.loadOp = vk::AttachmentLoadOp::eClear,
+			.storeOp = vk::AttachmentStoreOp::eStore,
+			.clearValue = clearColor
+		};
+
+		// Set up the rendering info
+		vk::RenderingInfo renderingInfo = 
+		{
+			.renderArea = 
+			{
+				.offset = 
+			{
+				0,
+				0 
+			   },
+				.extent = swapChainExtent 
+			},
+			.layerCount = 1,
+			.colorAttachmentCount = 1,
+			.pColorAttachments = &attachmentInfo
+		};
+
+		// Begin rendering
+		commandBuffer.beginRendering(renderingInfo);
+
+		// Rendering commands will go here
+
+		// End rendering
+		commandBuffer.endRendering();
+
+		// Transition the image layout for presentation
+		transition_image_layout(
+			imageIndex,
+			vk::ImageLayout::eColorAttachmentOptimal,
+			vk::ImageLayout::ePresentSrcKHR,
+			vk::AccessFlagBits2::eColorAttachmentWrite,
+			{},
+			vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+			vk::PipelineStageFlagBits2::eBottomOfPipe
+		);
+
+		commandBuffer.end();
+	}
+	void createCommandPool()
+	{
+		vk::CommandPoolCreateInfo poolInfo
+		{ 
+			.flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+			.queueFamilyIndex = queueIndex 
+		};
+
+		commandPool = vk::raii::CommandPool(device, poolInfo);
+	}
+	void createCommandBuffer()
+	{
+		vk::CommandBufferAllocateInfo allocInfo
+		{
+			.commandPool = commandPool,
+			.level = vk::CommandBufferLevel::ePrimary,
+			.commandBufferCount = 1
+		};
+
+		commandBuffer = std::move(vk::raii::CommandBuffers(device, allocInfo).front());
+	}
 	void initVulkan()
 	{
 		createInstance();
@@ -729,6 +858,8 @@ private:
 		createSwapChain();
 		createImageViews();
 		createGraphicsPipeline();
+		createCommandPool();
+		createCommandBuffer();
 	}
 
 	void mainLoop()
